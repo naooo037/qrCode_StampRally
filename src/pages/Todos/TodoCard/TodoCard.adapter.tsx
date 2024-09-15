4931f102fe08ddccecfac7e30af29a4142060919
itojun-1230 <juntou1230@gmail.com>
2024-09-15 23:41:40 +0900

    update: あちこちで使いそうなのでdateFormateをfeatureへ移動

--- src/pages/Todos/TodoCard/TodoCard.adapter.tsx
+++ src/pages/Todos/TodoCard/TodoCard.adapter.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 
 import { TodoSchema } from '@/.openapi/api'
 
+import { dateFormate } from '@/feature/dateFormate'
 import { useUpdateComplatedTodoAction } from '@/hooks/useUpdateComplatedTodoAction'
 
 import { TodoCard } from './TodoCard'
@@ -19,9 +20,4 @@ export const TodoCardAdapter: FC<Props> = ({ todo }) => {
 	}
 
 	return <TodoCard todo={todo} handleUpdateCompleate={handleUpdateCompleate} date={date} />
-}
-
-const dateFormate = (date: string) => {
-	const d = new Date(date)
-	return `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()} ${d.getHours()}:${d.getMinutes()}`
 }
