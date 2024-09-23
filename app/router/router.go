0a2f65fe85304076612ea53ea1731f159d3ea144
itojun-1230 <juntou1230@gmail.com>
2024-09-23 23:59:38 +0900

    delete: todoに関するプログラム

--- app/router/router.go
+++ app/router/router.go
@@ -1,27 +1,17 @@
 package router
 
 import (
-	"github.com/itojun-1230/go_react_template/app/router/hello"
-	"github.com/itojun-1230/go_react_template/app/router/hellos"
-	"github.com/itojun-1230/go_react_template/app/router/todo"
-	"github.com/itojun-1230/go_react_template/app/router/todo/id"
-
 	"github.com/gin-gonic/gin"
+	"github.com/itojun-1230/go_react_template/app/router/users"
 	"gorm.io/gorm"
 )
 
 func Router(db *gorm.DB) (*gin.Engine) {
 	engine := gin.Default()
 
-	engine.GET("/api/hello", hello.GetHello)
-	engine.GET("/api/hellos", hellos.GetHellos)
-
-	engine.GET("/api/todo", todo.GetTodos)
-	engine.POST("/api/todo", todo.PostTodo)
-	engine.GET("/api/todo/:id", todoById.GetTodoById)
-	engine.DELETE("/api/todo/:id", todoById.DeleteTodoById)
-	engine.PUT("/api/todo/:id", todoById.PutTodoById)
-	
+	engine.POST("/api/users/register", users.RegisterUser)
+	engine.GET("/api/users/:user_id", users.GetUserById)
+	engine.GET("/api/users/:user_id/stamps", users.GetUserCollectStamps)
 
 	return engine
 }
