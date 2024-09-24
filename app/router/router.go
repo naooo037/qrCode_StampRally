3f14d4fff861394f78ce31257259bda3d2237ba2
itojun-1230 <juntou1230@gmail.com>
2024-09-24 09:46:02 +0900

    add: ralliesのエンドポイント

--- app/router/router.go
+++ app/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/itojun-1230/go_react_template/app/router/rallies"
 	"github.com/itojun-1230/go_react_template/app/router/users"
 	"gorm.io/gorm"
 )
@@ -15,6 +16,9 @@ func Router(db *gorm.DB) (*gin.Engine) {
 	engine.GET("/api/users/:user_id", users.GetUserById)
 	engine.DELETE("/api/users/:user_id", users.DeleteUser)
 	engine.GET("/api/users/:user_id/stamps", users.GetUserCollectStamps)
+
+	engine.GET("/api/rallies", rallies.GetRallies)
+	engine.POST("/api/rallies", rallies.PostRallies)
 
 	return engine
 }
