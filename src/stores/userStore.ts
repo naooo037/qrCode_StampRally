64d2e432197786e32ebbaca87d1e51b269037391
itojun-1230 <juntou1230@gmail.com>
2024-09-26 09:38:54 +0900

    update: userStore 各種情報を追加

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -2,14 +2,22 @@ import { create } from 'zustand';
 
 type State = {
   userId: string | undefined;
+  name: string;
+  rallyId: string | undefined;
 };
 
 type Action = {
   setUserId: (userId: string) => void;
+  setName: (name: string) => void;
+  setRallyId: (rallyId: string) => void;
 };
 
 export const useTodoStore = create<State & Action>()((set) => ({
   userId: undefined,
+  name: "名無しさん",
+  rallyId: undefined,
 
   setUserId: (userId) => set({ userId }),
+  setName: (name) => set({ name }),
+  setRallyId: (rallyId) => set({ rallyId }),
 }));
