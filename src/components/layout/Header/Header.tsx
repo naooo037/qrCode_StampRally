c878e3c1b56f1a1f0d194f528c90c866b49a282e
naokush420 <t5422036@fukuyama-u.ac.jp>
2024-10-01 16:18:17 +0900

    ヘッダーに名前を表示

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,15 +1,22 @@
 import { FC } from 'react'
-import { Header } from 'smarthr-ui'
+import { useCookies } from 'react-cookie'
+import { Button, Header, HeaderDropdownMenuButton } from 'smarthr-ui'
 import styled from 'styled-components'
 
 import { Logo } from './Logo'
 
 export const HeaderComponent: FC = () => {
+	const [cookies] = useCookies()
+
 	return (
 		<MyHeader
 			logo={<Logo />}
 			tenants={[{ id: 'template', name: 'QRコード スタンプラリー in 福山大学' }]}
-		></MyHeader>
+		>
+			<HeaderDropdownMenuButton label={cookies.name}>
+				<Button>migrate</Button>
+			</HeaderDropdownMenuButton>
+		</MyHeader>
 	)
 }
 const MyHeader = styled(Header)`
